29c7f3e2b8f8b1ee7380ff845d67b7a0cfd01033
Pavle <102835309+mpavle1@users.noreply.github.com>
2024-12-08 02:47:57 +0100

    day 3 cleanup

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -9,19 +9,19 @@ import (
 
 func main() {
 	data, _ := os.ReadFile("input2.txt")
-	parsedText := string(data)
+	inputText := string(data)
 
 	numberReg := regexp.MustCompile(`\d{1,3}`)
-	reg := regexp.MustCompile(`mul\(\d{1,3}\,\d{1,3}\)`)
+	mulReg := regexp.MustCompile(`mul\(\d{1,3}\,\d{1,3}\)`)
 
-	matches := reg.FindAllString(parsedText, -1)
+	matches := mulReg.FindAllString(inputText, -1)
 
 	sum := 0
-	for i := 0; i < len(matches); i++ {
-		numberMatches := numberReg.FindAllString(matches[i], -1)
+	for _, match := range matches {
+		numbers := numberReg.FindAllString(match, -1)
 
-		firstNum, _ := strconv.Atoi(numberMatches[0])
-		secondNum, _ := strconv.Atoi(numberMatches[1])
+		firstNum, _ := strconv.Atoi(numbers[0])
+		secondNum, _ := strconv.Atoi(numbers[1])
 
 		sum += firstNum * secondNum
 	}
